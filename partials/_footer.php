8156aa7039b366c548c76c49528a67e57e620c44
lekkha <lekhasoni001@gmail.com>
2021-12-12 10:53:42 +0530

    Add files via upload

---
+++ partials/_footer.php
@@ -0,0 +1,76 @@
+<?php 
+
+echo'<footer class="bg-dark text-center text-white  ms-0 me-0">
+  <!-- Grid container -->
+  <div class="container p-4 pb-0">
+    <!-- Section: Social media -->
+    <section class="mb-4">
+
+      <!-- github -->
+      <a class="btn btn-link btn-floating btn-lg text-light m-1" href="#!" role="button"
+        ><i class="bi-github" role="img" aria-label="GitHub"></i></a>
+
+      <!-- linkedin -->
+      <a class="btn btn-link btn-floating btn-lg text-light m-1" href="#!" role="button"
+        ><i class="bi bi-linkedin"></i></i></a>
+
+      <!-- Instagram -->
+      <a class="btn btn-link btn-floating btn-lg text-light m-1" href="#!" role="button">
+      <i class="bi bi-instagram"></i></a>
+
+    </section>
+    <!-- Section: Social media -->
+  </div>
+  <!-- Grid container -->
+
+
+  <!-- Grid container -->
+  <div class="container p-1 pb-0">
+    <!-- Section: Form -->
+    <section class="">
+      <form action="">
+        <!--Grid row-->
+        <div class="row d-flex justify-content-center">
+          <!--Grid column-->
+          <div class="col-auto">
+            <p class="pt-2">
+              Report offensive content
+            </p>
+          </div>
+          <!--Grid column-->
+
+          <!--Grid column-->
+          <div class="col-md-2 col-12">
+            <!-- Email input -->
+            <div class="form-outline mb-4">
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter your email">
+            <small id="emailHelp" class="form-text text-muted">We will get back to you.</small>
+          </div>
+          </div>
+          <!--Grid column-->
+
+          <!--Grid column-->
+          <div class="col-auto">
+            <!-- Submit button -->
+            <button type="submit" class="btn btn-outline-info mb-4">
+              submit
+            </button>
+          </div>
+          <!--Grid column-->
+        </div>
+        <!--Grid row-->
+      </form>
+    </section>
+    <!-- Section: Form -->
+  </div>
+  <!-- Grid container -->
+
+
+  <!-- Copyright -->
+  <div class="text-center p-1" style="background-color: rgba(0, 0, 0, 0.2);">
+    Copyright NSUT Forum 2021 | All Rights Reserved
+  </div>
+  <!-- Copyright -->
+</footer>
+'
+?>
---
+++ partials/_signupmodal.php
@@ -0,0 +1,38 @@
+<!-- Modal -->
+<div class="modal fade" id="signupmodal" tabindex="-1" aria-labelledby="signupmodalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="signupmodalLabel">Sign up for an account on NSUT Forums</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form action="/forum/partials/_handleSignup.php" method="post">
+                <div class="modal-body">
+
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Username</label>
+                        <!-- <input type="email" class="form-control" id="signupEmail" name="signupEmail" aria-describedby="emailHelp"> -->
+                        <input type="text" class="form-control" id="signupEmail" name="signupEmail" aria-describedby="emailHelp">
+                        <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="signupPassword" name="signupPassword">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Confirm Password</label>
+                        <input type="password" class="form-control" id="signupcPassword" name="signupcPassword">
+                    </div>
+                   
+                    <button type="submit" class="btn btn-primary">Submit</button>
+
+                </div>
+
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
